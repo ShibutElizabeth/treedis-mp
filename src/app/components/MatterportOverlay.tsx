6ce55595330c40050dc9b7d98df0716c661c04fd
ShibutElizabeth <ellis.shybut@mail.ru>
2025-03-03 18:29:21 +0800

    feat: added styles

--- src/app/components/MatterportOverlay.tsx
+++ src/app/components/MatterportOverlay.tsx
@@ -3,28 +3,20 @@
 import { useRef } from "react";
 import { useMatterportScene } from "../hooks/useMatterportScene";
 import { MatterportContext } from "./MatterportContext";
+import styles from "@/app/page.module.css";
 
 const MatterportOverlay = ({ children }: { children: React.ReactNode | null }) => {
     const iframeRef = useRef<HTMLIFrameElement | null>(null);
     const matterportScene = useMatterportScene(iframeRef);
 
     return (
-        <div
-            style={{
-            position: "relative",
-            width: "100vw",
-            height: "100vh"
-        }}>
+        <div className={styles.container}>
             <MatterportContext.Provider value={matterportScene}>{children}</MatterportContext.Provider>
             <iframe
                 id="showcase"
                 ref={iframeRef}
                 src={`/showcase-bundle/showcase.html?m=${process.env.NEXT_PUBLIC_MP_MODEL_ID}&applicationKey=${process.env.NEXT_PUBLIC_MP_SDK_KEY}`}
-                style={{
-                    width: "100%",
-                    height: "100%",
-                    border: "none"
-                }}
+                className={styles.scene}
                 allow="vr"
                 allowFullScreen></iframe>
         </div>
