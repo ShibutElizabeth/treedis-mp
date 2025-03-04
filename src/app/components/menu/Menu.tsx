419ad57ad09f29c62551d2892fc8dcb6a27f44f4
ShibutElizabeth <ellis.shybut@mail.ru>
2025-03-04 21:18:18 +0800

    feat: menu search & hide

--- src/app/components/menu/Menu.tsx
+++ src/app/components/menu/Menu.tsx
@@ -2,10 +2,15 @@
 
 import { MenuItem, ToOffice } from "@/app/types/utils";
 import styles from "./menu.module.css";
-import { JSX } from "react";
+import { useEffect, useRef, useState } from "react";
 import { MenuOption } from "./MenuOption";
+import { useMatterportContext } from "@/app/hooks/UseMatterportContext";
 
 export const Menu = () => {
+    const { sdk } = useMatterportContext();
+    const [searchTerm, setSearchTerm] = useState("");
+    const [isOpen, setIsOpen] = useState(false);
+    const menuRef = useRef<HTMLDivElement | null>(null);
     const menuItemsData: MenuItem[] = [
         {
             title: 'Teleport to office',
@@ -17,20 +22,51 @@ export const Menu = () => {
         }
     ];
 
-    const getItems = () => {
-        const menuItems: JSX.Element[] = [];
-        menuItemsData.forEach((item, i) => {
-            menuItems.push(
-                <MenuOption key={item.title + i} item={item} />
-            )
-        });
+    const filteredItems = menuItemsData.filter(item =>
+        item.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
-        return menuItems;
+    const getItems = () => {
+        return filteredItems.map((item, i) => (
+            <MenuOption key={item.title + i} item={item} />
+        ));
+    };
+
+    const toggleMenu = () => {
+        if(menuRef.current){
+            menuRef.current.style.transform = isOpen ? 'translateY(-83%)' : 'translateY(0%)';
+            setIsOpen(!isOpen);
+        }
     }
 
+    useEffect(() => {
+        if(sdk && menuRef.current){
+            sdk.App.state.subscribe((state) => {
+                if(state.phase === sdk.App.Phase.PLAYING){
+                    menuRef.current.style.display = 'flex';
+                    toggleMenu();
+                } else {
+                    menuRef.current.style.display = 'none';
+                }
+            })
+        }
+    }, [menuRef, sdk])
+
     return (
-        <div className={styles.menuContainer}>
-            { getItems() }
+        <div ref={menuRef} className={styles.menu}>
+            <div className={styles.menuContainer}>
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    className={styles.searchBar}
+                />
+                { getItems() }
+                <div className={styles.hideBar} onClick={() => toggleMenu()}>
+                    <img className={isOpen ? styles.up : ''} src="/Arrow.svg" alt=""/>
+                </div>
+            </div>
         </div>
     );
 };
